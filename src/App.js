43c24eb7f49c39b38b7e2bf7e663b85e869a2761
Tom Ko <yungsu2391@gmail.com>
2022-12-01 09:11:40 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let post = "강남 우동 맛집";
+  let [a,b] = useState('남자 코트 추천');
+
   return (
     <div className="App">
       <div className="black-nav">
-        blog
+        <h4 style={{color:'red', fontSize:"16px"}}>blog</h4>
+      </div>
+      <div className="list">
+        <h4>{ a }</h4>
+        <p>2월 17일 발행</p>
+        <br></br>
+      </div>
+      <div className="list">
+        <h4>{ a }</h4>
+        <p>2월 17일 발행</p>
       </div>
     </div>
   );
